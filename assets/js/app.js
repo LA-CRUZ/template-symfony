f6ef3d402be18da3e97a9dc891ee46ecdb0f95a7
Lucas Audart <audart@MacBook-Pro-de-Lucas.local>
2019-10-18 18:44:09 +0200

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+// any CSS you require will output into a single css file (app.css in this case)
+require('../css/app.css');
+
+// any SCSS file you require will output in one css file
+require('../scss/main.scss');
+
+// any JS file you require
