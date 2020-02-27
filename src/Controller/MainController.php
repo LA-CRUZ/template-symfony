94b173bba81ffcb54b1525765692227121778008
AUDART <lucas.audart@etu.univ-lyon1.fr>
2020-02-27 13:50:15 +0100

    Removing Translation and adding Error support

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,13 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/{_locale}")
- */
 class MainController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function index()
     {
